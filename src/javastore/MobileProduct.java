39214b10364b53c3d4e5485dbaea741f6b808e42
3oxic <3oxic@192.168.1.5>
2016-04-21 23:49:26 +0300

    Merge origin/master
    Conflicts:
    	src/javastore/ComputerProduct.java
    	src/javastore/Product.java

---
+++ src/javastore/MobileProduct.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastore;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author 3oxic
+ */
+public class MobileProduct extends Product{
+
+    private String os;
+    private String monitor;
+    private String processor;
+    private String camera;
+    private String wifi;
+    private Color colour = new Color();
+    
+    
+    public MobileProduct()
+    {
+        super();
+        this.os = "";
+        this.monitor="";
+        this.processor="";
+        this.camera = "";
+        this.wifi="";
+        this.colour= new Color();
+        
+    }
+    public MobileProduct(String name,String details,int code,float price,Image[] images,String os,String monitor,String processor,String camera,String wifi,Color colour)
+    {
+        super(name,details,code,price,images);
+        this.os=os;
+        this.monitor=monitor;
+        this.processor=processor;
+        this.camera=camera;
+        this.wifi=wifi;
+        this.colour=colour;
+        
+    }
+}
+
