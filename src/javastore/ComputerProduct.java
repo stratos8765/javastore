e2254d8b838de2104d554cf13b958c44b47782f7
3oxic <3oxic@192.168.1.5>
2016-04-21 22:07:59 +0300

    new commit

---
+++ src/javastore/ComputerProduct.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastore;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author 3oxic
+ */
+public class ComputerProduct extends Product{
+    //Επεξεργαστής, Μνήμη, Κάρτα Γραφικών, Οθόνη, Λειτουργικό
+    private String processor;
+    private String ram;
+    private String graphicscard;
+    private String monitor;
+    private String os;
+    public ComputerProduct()
+    {
+        super();
+        this.processor="";
+        this.ram="";
+        this.graphicscard="";
+        this.monitor="";
+        this.os="";
+        
+    }
+    public ComputerProduct(String name,String details,int code,float price,Image[] images,String processor,String ram,String graphicscard,String monitor,String os)
+    {
+        super(name,details,code,price,images);
+        this.processor=processor;
+        this.ram=ram;
+        this.graphicscard=graphicscard;
+        this.monitor=monitor;
+        this.os=os;
+    }
+}
---
+++ src/javastore/Product.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastore;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author 3oxic
+ */
+public abstract class Product {
+    private String name;
+    private String details;
+    private int code;
+    private float price;
+    private Image[] images = new Image[20];
+    
+        //Constructors
+    public Product()
+    {
+        this.name="";
+        this.details="";
+        this.code = 0;
+        this.price = 0;
+        
+    }
+    public Product(String name,String details,int code,float price,Image[] images)
+    {
+        this.name=name;
+        this.details=details;
+        this.code=code;
+        this.price=price;
+        this.images=images;
+        
+    }
+    public void SetName(String name)
+    {
+        this.name=name;
+    }
+    public void SetDetails(String details)
+    {
+        this.details=details;
+        
+    }
+    public void SetCode(int code)
+    {
+        this.code=code;
+        
+    }
+    public void SetPrice(float price)
+    {
+        this.price=price;
+        
+    }
+    public void SetImages(Image[] images)
+    {
+        this.images=images;
+        
+    }
+}
