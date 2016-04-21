80dab48242ebe0065c4c934c9a880f87b5aff289
3oxic <3oxic@192.168.1.5>
2016-04-21 23:50:34 +0300

    Merge origin/master
    Conflicts:
    	src/javastore/ComputerProduct.java
    	src/javastore/Product.java

---
+++ src/javastore/Color.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastore;
+
+/**
+ *
+ * @author 3oxic
+ */
+public class Color {
+    private int red;
+    private int green;
+    private int blue;
+    private String str;
+    
+   public Color() 
+   {
+       this.red=0;
+       this.green=0;
+       this.blue=0;
+       
+   }
+   public Color(int red,int green,int blue)
+   {
+   this.red=red;
+   this.green=green;
+   this.blue=blue;
+   
+   }
+   public void SetGreen(int green)
+   {
+       this.green=green;
+       
+   }
+      public void SetRed(int red)
+   {
+       this.red=red;
+       
+   }
+         public void SetBlue(int blue)
+   {
+       this.blue=blue;
+       
+   }
+  public int GetGreen()
+  {
+      return this.green;
+      
+  }
+    public int GetBlue()
+  {
+      return this.blue;
+      
+  }
+      public int GetRed()
+  {
+      return this.red;
+      
+  }
+      public String toString()
+      {
+       str="Red:"+red+"Green:"+green+"Blue:"+blue;
+       return str;
+       
+      }
+   
+}
