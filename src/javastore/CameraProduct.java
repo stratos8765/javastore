8673ebf651d3b800c3c13333893174c0a8392090
3oxic <3oxic@192.168.1.5>
2016-04-22 04:09:27 +0300

    Merge origin/master
    Conflicts:
    	src/javastore/ComputerProduct.java
    	src/javastore/Product.java

---
+++ src/javastore/CameraProduct.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastore;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author 3oxic
+ */
+public class CameraProduct extends Product {
+ private int megapixels;
+ private int iso;
+ private int exposure;
+ private int fps;
+ //constructos 
+ public CameraProduct()
+ {
+     super();
+     this.megapixels=0;
+     this.iso=0;
+     this.exposure=0;
+     this.fps=0;
+     
+ }
+ public CameraProduct(String name,String details,int code,float price,Image[] images,int megapixels,int iso,int exposure,int fps)
+ {
+  super(name,details,code,price,images);
+  this.megapixels=megapixels;
+  this.iso=iso;
+  this.exposure=exposure;
+  this.fps=fps;
+  
+ }
+ //end constructors
+ //setters
+ public void SetMegaPixels(int megapixels)
+ {
+     this.megapixels=megapixels;
+     
+ }
+ public void SetIso(int iso)
+ {
+     this.iso=iso;
+     
+ }
+ public void SetExposure(int exposure)
+ {
+     this.exposure=exposure;
+     
+ }
+ public void SetFps(int fps)
+ {
+     this.fps=fps;
+     
+ }
+ //end Setters
+ //getters
+
+ public int GetMexaPixels()
+ {
+     return this.megapixels;
+     
+ }
+ public int GetIso()
+ {
+     return this.iso;
+     
+ }
+ public int GetExposure()
+ {
+     return this.exposure;
+     
+ }
+ public int Fps()
+ {
+     return this.fps;
+     
+ }
+ //end getters
+}
